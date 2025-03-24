8051a5294375e4c7cdc9ea80613a583c97abd474
youngmin63 <kym827432@gmail.com>
2025-03-24 15:53:56 +0900

    Update script2.js

--- script2.js
+++ script2.js
@@ -51,7 +51,7 @@ stopBtn.onclick = () => {
     return;
   }
 
-  fetch("https://youngmin63.github.io/balancetestWeb//api/test", {
+  fetch("https://youngmin63.github.io/balancetestWeb:8080/api/test", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
