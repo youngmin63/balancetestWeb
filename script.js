5158014a06fa082ea1d4508f6383817655cadf4f
youngmin63 <kym827432@gmail.com>
2025-03-25 14:20:17 +0900

    Update script.js

--- script.js
+++ script.js
@@ -39,7 +39,7 @@ stopBtn.onclick = () => {
     return;
   }
 
-  fetch("https://balancetest-backend.onrender.com", {
+  fetch("postgresql://dpg-cvh3apdrie7s73ejlhrg-a.singapore-postgres.render.com/balance_test_db", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({
